f3529d8176cd10de48e8f23ebc766ade55169700
toky <979564561@qq.com>
2018-10-04 23:53:08 +0800

    第 6 天

---
+++ x6 day/prcDecorator/PrcDecorator.py
@@ -0,0 +1,12 @@
+import functools
+
+
+def log(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kw):
+        print('call %s():' % func.__name__)
+        return func(*args, **kw)
+    return wrapper
+
+
+
